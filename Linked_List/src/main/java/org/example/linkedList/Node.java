a6fe088684c80a5888b9d191c911a33344b3bd72
Kiran Chauhan <kc7563330@gmail.com>
2026-01-06 21:30:42 +0530

    Deletion in LinkedList

--- Linked_List/src/main/java/org/example/linkedList/Node.java
+++
@@ -1,4 +0,0 @@
-package org.example.linkedList;
-
-public class Node {
-}
---
+++ Linked_List/src/main/java/org/example/singlyLinkedList/Node.java
@@ -0,0 +1,11 @@
+package org.example.linkedList;
+
+class Node {
+
+    int value;
+    Node next;
+
+    Node(int value) {
+        this.value = value;
+    }
+}
